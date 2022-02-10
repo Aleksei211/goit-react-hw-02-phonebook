d8b813366758915b6163929560ad2a6e38d2ba4c
Aleksei211 <85795990+Aleksei211@users.noreply.github.com>
2022-02-10 23:07:40 +0200

    task-2

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import Phonebook from "./components/Phonebook/Phonebook";
+import contacts from './components/Contacts/contacts.json'
+import Filter from "./components/Filter/Filter";
+import Contacts from "./components/Contacts";
+import shortid from "shortid";
+import s from './components/Contacts/Contacts.module.css'
+
+//model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
+
+
+class App extends Component{
+  state = {
+  contacts: contacts,
+  filter: '',
+
+  };
+
+  addContact = values => {
+    const { contacts } = this.state;
+    const repeated = contacts.find(contact => {
+      return contact.name.toLowerCase() === values.name.toLowerCase();
+    });
+    if (repeated) {
+      alert(`${values.name} is already in contacts`);
+      return;
+    }
+
+    const contact = {
+      id: shortid.generate(),
+      name: values.name,
+      number: values.number
+    };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }))
+   
+    
+    values.name = '';
+    values.number = '';
+  };
+
+  	deleteContact = cardId => {
+		this.setState(prevState => ({
+			contacts: prevState.contacts.filter(contact => contact.id !== cardId),
+		}));
+    };
+  
+  	filterChange = e => {
+		this.setState({ filter: e.target.value });
+	};
+
+  render() {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContact = contacts.filter(contact =>
+			contact.name.toLowerCase().includes(normalizedFilter),
+		)
+    return (
+      <div className={s.container}>
+        <h2 >Phonebook</h2>
+        
+        <Phonebook addContact={this.addContact} />
+        
+        <Filter onFilterChange={this.filterChange} value={filter} />
+
+        <Contacts contacts={visibleContact} deleteContact={this.deleteContact} />
+        
+      </div> 
+      
+    )
+  }
+}
+
+export default App;
